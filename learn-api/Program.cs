8fbb8952838002d992ce387285921565ddeecd79
queens kisivuli <queenskisivuli@gmail.com>
2023-08-29 22:57:59 +0300

    db config

--- learn-api/Program.cs
+++ learn-api/Program.cs
@@ -1,3 +1,6 @@
+using learn_api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Connect to db
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection"));
+});
 
 var app = builder.Build();
 
