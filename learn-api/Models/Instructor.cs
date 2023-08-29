470ce79fe7cb56332540bdaf182582a6c67bf8dd
queens kisivuli <queenskisivuli@gmail.com>
2023-08-29 22:04:18 +0300

    user instructions

---
+++ learn-api/Models/Instructor.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learn_api.Models
+{
+    public class Instructor : Person
+    {
+        public List<Course> Courses { get; set; } = new List<Course>();
+    }
+}
